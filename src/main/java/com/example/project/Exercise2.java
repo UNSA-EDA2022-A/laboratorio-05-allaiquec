279ec36aa105999be7a2e74d5e10fbbf43f5f361
allaiquec <88158031+allaiquec@users.noreply.github.com>
2022-06-14 00:01:25 -0500

    prueba2-ejer2

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -14,8 +14,33 @@ public class Exercise2 {
 
     public boolean existenDuplicados(String str) {
         MyStack<Character> stack = new LinkedListStack<>();
-        // Colocar codigo aqui
+         if(str == null) {
+        	return false;
+        }
+        
+        //Leer letra por letra, y guardar los abiertos
+        for (int i = 0; i < str.length(); i++) {
+        	////¿Es abierto o es una letra? Guardarlo
+            if (str.charAt(i) != ')') 
+                stack.push(str.charAt(i));
+            
+            //¿Es cerrado? Verificar si el ultimo elemento de la pila es abierto           
+            else {
+                //Si encontramos un parentesis abierto al iniciar, 
+            	  //nos damos cuenta de que ya está duplicado
+                if (stack.top() == '(') 
+                    return true;
 
+                //Buscaremos el parentesis abierto, para eso iremos eliminando
+                  //las expresiones que estan en la pila hasta encontrarlo
+                while (stack.top() != '(') 
+                    stack.pop();
+
+                //Una vez encontrado el parentesis abierto, lo eliminamos
+                stack.pop(); 
+            }
+        }
+        //Luego del recorrido damos con que la expresión no tiene ningún duplicado
         return false;
     }
 }
