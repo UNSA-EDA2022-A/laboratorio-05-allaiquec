78ae54f64c2eb57e1f053859e3bfce108a5fb12c
allaiquec <88158031+allaiquec@users.noreply.github.com>
2022-06-14 00:00:23 -0500

    prueba1-ejer1

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -14,9 +14,52 @@ public class Exercise1 {
     }
 
     public boolean esBalanceado(String s) {
-        MyStack<Character> stack = new LinkedListStack<>();
-        // Colocar codigo aqui
-
-        return false;
+         MyStack<Character> stack = new LinkedListStack<>();
+        
+        //Si esta vacio
+        if(s == null) {
+        	return true;
+        }
+        
+        //Leer letra por letra, y guardar los abiertos
+        for (int i=0; i<s.length(); i++) {
+        	   
+        	//¿Es abierto? Guardarlo
+        	if(s.charAt(i) == '(' || s.charAt(i) == '{' ||s.charAt(i) == '[') 
+        		   stack.push(s.charAt(i));
+        	
+        	//¿Es cerrado? Verificar si el ultimo elemento de la pila es abierto
+        	else {
+        		//si es el primer elemento para verificar y es cerrado, pues ya esta mal
+        		if(i==0) 
+        			return false;
+        		//Verificar si el ultimo elemento de la pila es del mismo tipo
+        		else {
+        			if(Tipo(s.charAt(i)) == Tipo(stack.top())) 
+        				//se elimina el ultimo elemento
+        				stack.pop(); 	   
+        		}
+        	}
+        	   
+        	
+        	   
+        }
+      //Si la pila esta vacia, es correcto
+       return stack.isEmpty();
+        	
+        
+    }
+    //Definir que tipo es 
+    public char Tipo(char a) {
+    	//parentesis (p)
+    	if(a == '(' || a == ')' ) 
+    		return 'p';
+    	//corchete (c)
+    	else if(a == '[' || a == ']')
+    		return 'c';
+    	// llave (l)
+    	else 
+    		return 'l';
+    		
     }
 }
